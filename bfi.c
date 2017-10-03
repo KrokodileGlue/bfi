9e5c924d70ab36545f3432bf685b176963e1b666
KrokodileGlue <KrokodileGlue@outlook.com>
2017-10-03 16:40:03 -0700

    clean up a bunch of shit

--- bfi.c
+++ bfi.c
@@ -1,159 +1,119 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <time.h>
+#include <inttypes.h>
 
 /* UTILITIES =============================================================== */
-void fatal_error(const char* msg)
+#ifdef DEBUG
+#define DBG(...) fprintf(stdout, __VA_ARGS__);
+#else
+#define DBG(...)
+#endif
+
+#define USAGE "Usage: bfi INPUT_FILE -oOUTPUT_FILE\n"
+
+void die(const char *msg)
 {
 	if (msg)
-		puts(msg);
+		printf("\nbfi: error: %s", msg);
 
 	exit(EXIT_FAILURE);
 }
 
-void* bfi_malloc(size_t bytes)
+char *load_file(const char *path)
 {
-	void *res = malloc(bytes);
-	
-	if (!res)
-		fatal_error("out of memory.");
+	char *buf = NULL;
+	FILE *file = fopen(path, "r");
 
-	return res;
-}
+	if (!file) die("input file could not be opened for reading");
 
-void* bfi_realloc(void *block, size_t bytes)
-{
-	block = realloc(block, bytes);
-	
-	if (!block)
-		fatal_error("out of memory.");
+	if (!fseek(file, 0L, SEEK_END)) {
+		long len = ftell(file);
+		if (len == -1) return NULL;
 
-	return block;
-}
+		buf = malloc(len + 1);
 
-char* load_file(const char* path)
-{
-	char* buf = NULL;
-	FILE* file = fopen(path, "r");
-	
-	if (file) {
-		if (fseek(file, 0L, SEEK_END) == 0) {
-			long len = ftell(file);
-			if (len == -1) return NULL;
-			
-			buf = bfi_malloc(len + 1);
+		if (fseek(file, 0L, SEEK_SET) != 0)
+			return NULL;
 
-			if (fseek(file, 0L, SEEK_SET) != 0)
-				return NULL;
+		size_t new_len = fread(buf, 1, len, file);
+		if (ferror(file) != 0)
+			die("input file could not be read");
 
-			size_t new_len = fread(buf, 1, len, file);
-			if (ferror(file) != 0) {
-				fatal_error("Input file could not be read.\n");
-			} else {
-				buf[new_len++] = '\0';
-			}
-		}
-		
-		fclose(file);
+		buf[new_len++] = 0;
 	}
-	
+
+	fclose(file);
+
 	return buf;
 }
 
-#define IS_BF_COMMAND(c)         \
-	(c == '<' || c == '>'    \
-	|| c == '-' || c == '+') \
-	|| c == ',' || c == '.'  \
-	|| c == '[' || c == ']'
-
-void sanitize(char* str)
-{
-#define SANITIZER_IS_BF_COMMAND(c) (   \
-        c == '<' || c == '>'     \
+#define IS_BF_COMMAND(c) (       \
+           c == '<' || c == '>'  \
         || c == '+' || c == '-'  \
         || c == '[' || c == ']'  \
         || c == ',' || c == '.')
 
-#define SANITIZER_ADD(a, c) \
-        if (a >= 0) { \
-                for (int counter = 0; counter < abs(a); counter++) { \
-                        *c++ = '+'; \
-                } \
-        } else { \
-    	        for (int counter = 0; counter < abs(a); counter++) { \
-                        *c++ = '-'; \
-                } \
-        }
+#define ADD(a,c)                             \
+	for (int i = 0; i < abs(a); i++)     \
+		*c++ = (a) >= 0 ? '+' : '-'; \
 
-#define SANITIZER_MOVE_PTR(a, c) \
-    if (a >= 0) { \
-        for (int counter = 0; counter < abs(a); counter++) { \
-            *c++ = '>'; \
-        } \
-    } else { \
-    	for (int counter = 0; counter < abs(a); counter++) { \
-            *c++ = '<'; \
-        } \
-    }
+#define MOVPTR(a,c)                          \
+	for (int i = 0; i < abs(a); i++)     \
+		*c++ = (a) >= 0 ? '>' : '<'; \
 
-#define SANITIZER_IS_CONTRACTABLE(c) (     \
-        c == '<' || c == '>'     \
-        || c == '+' || c == '-')
+void sanitize(char* str)
+{
+	if (!str) return;
 
-	char* buf = malloc(strlen(str) + 1);
+	size_t starting_len = strlen(str);
+	char* i = str, *out = str;
 
-	size_t starting_len;
-	char* i, *out;
-
-	starting_len = strlen(str);
-	i = str, out = buf;
-
-	while (*i != '\0') {
-		if (SANITIZER_IS_CONTRACTABLE(*i)) {
+	while (*i) {
+		if (*i == '+' || *i == '-' || *i == '<' || *i == '>') {
 			if (*i == '+' || *i == '-') {
 				int sum = 0;
+
 				while (*i == '+' || *i == '-') {
 					if (*i == '+') sum++;
 					else sum--;
+
 					i++;
 				}
-				SANITIZER_ADD(sum, out)
+
+				ADD(sum, out)
 			} else {
 				int sum = 0;
+
 				while (*i == '>' || *i == '<') {
 					if (*i == '>') sum++;
 					else sum--;
+
 					i++;
 				}
-				SANITIZER_MOVE_PTR(sum, out)
+
+				MOVPTR(sum, out)
 			}
 		} else if (!strncmp(i, "][", 2)) {
 			i += 2;
 			int depth = 1;
-			while (*i != '\0' && depth) {
+			do {
+				i++;
 				if (*i == '[') depth++;
 				else if (*i == ']') depth--;
-				i++;
-			}
-			i--;
-		} else if (SANITIZER_IS_BF_COMMAND(*i)) {
-			*out++ = *i++;
-		} else {
-			i++;
+			} while (depth && *i);
 		}
+		else if (IS_BF_COMMAND(*i)) *out++ = *i++;
+		else i++;
 	}
-	*out = '\0';
-	strcpy(str, buf);
 
-	if (strlen(str) < starting_len) {
-		sanitize(str);
-	}
+	*out = 0;
+	if (strlen(str) < starting_len) sanitize(str);
 }
 
 /* COMPILATION ============================================================= */
 #ifdef DEBUG
-char instr_strings[12][20] = {
+const char *instr_str[12] = {
 	"ADD   ",
 	"SUB   ",
 	"SUBPTR",
@@ -169,23 +129,29 @@ char instr_strings[12][20] = {
 };
 #endif
 
-typedef struct {
+struct instr {
 	enum {
-		INSTR_ADD   , INSTR_SUB,
-		INSTR_SUBPTR, INSTR_ADDPTR,
-		INSTR_PUTCH , INSTR_GETCH,
-		INSTR_CJUMP , INSTR_JUMP,
-		INSTR_CLEAR , INSTR_MUL,
-		INSTR_END   , INSTR_DCLEAR /* dangerous clear*/
+		INSTR_ADD,
+		INSTR_SUB,
+		INSTR_SUBPTR,
+		INSTR_ADDPTR,
+		INSTR_PUTCH,
+		INSTR_GETCH,
+		INSTR_CJUMP,
+		INSTR_JUMP,
+		INSTR_CLEAR,
+		INSTR_DCLEAR,
+		INSTR_MUL,
+		INSTR_END
 	} type;
-	int data, offset;
-} Instruction;
-Instruction* program;
 
-Instruction make_instruction(int type, int data, int offset)
+	int data, offset;
+} *code;
+
+struct instr make_instruction(int type, int data, int offset)
 {
-	Instruction res;
-	
+	struct instr res;
+
 	res.type = type;
 	res.data = data;
 	res.offset = offset;
@@ -193,7 +159,7 @@ Instruction make_instruction(int type, int data, int offset)
 	return res;
 }
 
-char* bf_commands = "+-<>.,[]";
+char *bf_commands = "+-<>.,[]";
 int command_type(char command)
 {
 	for (int i = 0; i < (int)strlen(bf_commands); i++)
@@ -205,20 +171,20 @@ int command_type(char command)
 
 #define MAX_STACK_DEPTH 4096
 int stack[MAX_STACK_DEPTH], sp, ip;
-char* tok;
+char *tok;
 
 /* takes a pointer to the beginning and end of a piece of text
  * and turns it into an independant NUL-terminated string */
-char* form_string(char* begin, char* end)
+char *form_string(char *begin, char *end)
 {
-	char* str = bfi_malloc(end - begin + 15);
+	char *str = malloc(end - begin + 15);
 	strncpy(str, begin, end - begin + 1);
 	str[end - begin + 1] = 0;
 
 	return str;
 }
 
-int balanced_loop(const char* loop)
+int balanced_loop(const char *loop)
 {
 	int offset = 0;
 	for (int i = 0; i < (int)strlen(loop); i++) {
@@ -229,16 +195,16 @@ int balanced_loop(const char* loop)
 	return !offset;
 }
 
-typedef struct {
+struct loop_cell_info {
 	int offset, data;
-} LoopCellInformation;
+};
 
-typedef struct {
-	LoopCellInformation* loop;
+struct loop_info {
+	struct loop_cell_info *loop;
 	int num;
-} LoopInfo;
+};
 
-int find_cell_with_offset(LoopCellInformation* cells, int num_cells, int offset)
+int find_cell_with_offset(struct loop_cell_info *cells, int num_cells, int offset)
 {
 	for (int i = 0; i < num_cells; i++)
 		if (cells[i].offset == offset)
@@ -247,7 +213,7 @@ int find_cell_with_offset(LoopCellInformation* cells, int num_cells, int offset)
 	return -1;
 }
 
-int emit_multiplication_loops(LoopInfo* loop_info)
+int emit_multiplication_loops(struct loop_info *loop_info)
 {
 	int index_of_main_cell = find_cell_with_offset(loop_info->loop, loop_info->num, 0);
 	if (loop_info->loop[index_of_main_cell].data != -1)
@@ -255,24 +221,24 @@ int emit_multiplication_loops(LoopInfo* loop_info)
 
 	for (int i = 0; i < loop_info->num; i++) {
 		if (i != index_of_main_cell) {
-			program[ip++] = make_instruction(
+			code[ip++] = make_instruction(
 			INSTR_MUL,
 			loop_info->loop[i].data,
 			loop_info->loop[i].offset);
 		}
 	}
 
-	program[ip++] = make_instruction(INSTR_CLEAR, -1, -1);
-	
+	code[ip++] = make_instruction(INSTR_CLEAR, -1, -1);
+
 	free(loop_info->loop);
 	free(loop_info);
 
 	return 1;
 }
 
-LoopInfo* analyze_loop(const char* loop)
+struct loop_info *analyze_loop(const char *loop)
 {
-	LoopCellInformation* cells = bfi_malloc(strlen(loop) * sizeof(LoopCellInformation));
+	struct loop_cell_info *cells = malloc(strlen(loop) * sizeof *cells);
 	int num_cells = 0, offset = 0, amount = 0;
 
 	int i = 1;
@@ -302,13 +268,13 @@ LoopInfo* analyze_loop(const char* loop)
 	}
 
 #ifdef DEBUG
-	printf("loop: %s\nloop anaylsis:\n", loop);
+	DBG("loop: %s\n", loop);
 	for (int j = 0; j < num_cells; j++) {
-		printf("\toffset: [%4d] data: [%4d]\n", cells[j].offset, cells[j].data);
+		DBG("\toffset: [%4d] data: [%4d]\n", cells[j].offset, cells[j].data);
 	}
 #endif
 
-	LoopInfo* res = bfi_malloc(sizeof(LoopInfo));
+	struct loop_info *res = malloc(sizeof *res);
 	res->loop = cells;
 	res->num = num_cells;
 
@@ -323,38 +289,36 @@ int muliplication_loop()
 		return 0;
 
 	while (*end != ']') {
-		if (*end == '\0' || command_type(*end) >= 4)
+		if (!*end || command_type(*end) >= 4)
 			return 0;
 		end++;
 	}
 
-	/* if we've reached this point then the loop does not contain
-	 * any embedded loops or commands other than + - < and > */
-	
-	char* loop = form_string(begin, end);
+	/*
+	 * If we've reached this point then the loop does not contain
+	 * any embedded loops or commands other than + - < and >.
+	 */
+
+	char *loop = form_string(begin, end);
 	if (!balanced_loop(loop)) {
 		free(loop);
 		return 0;
 	}
 
-	/* if we've reached this point then the loop is balanced. */
-	LoopInfo* loop_info = analyze_loop(loop);
+	/*
+	 * If we've reached this point then the loop is balanced.
+	 */
+	struct loop_info *loop_info = analyze_loop(loop);
 
-#ifdef DEBUG
-	printf("\tloop passed as multiplication loop? ");
-#endif
+	DBG("\tmultiplication loop? ");
 
 	if (emit_multiplication_loops(loop_info)) {
-#ifdef DEBUG
-		printf("yes\n");
-#endif
+		DBG("yes\n");
 		tok = end + 1;
 		free(loop);
 		return 1;
 	} else {
-#ifdef DEBUG
-		printf("no\n");
-#endif
+		DBG("no\n");
 		free(loop);
 		return 0;
 	}
@@ -381,11 +345,11 @@ void contract()
 			while (*tok == '-' || *tok == '+') {
 				if (*tok == '-') data--;
 				else data++;
-				
+
 				tok++;
 			}
 
-			program[ip++] = make_instruction(INSTR_ADD, data, offset);
+			code[ip++] = make_instruction(INSTR_ADD, data, offset);
 
 			while (*tok == '<' || *tok == '>') {
 				if (*tok == '<') offset--;
@@ -395,7 +359,7 @@ void contract()
 			while (*tok == ',')
 				data++, tok++;
 
-			program[ip++] = make_instruction(INSTR_GETCH, data, offset);
+			code[ip++] = make_instruction(INSTR_GETCH, data, offset);
 
 			while (*tok == '<' || *tok == '>') {
 				if (*tok == '<') offset--;
@@ -405,7 +369,7 @@ void contract()
 			while (*tok == '.')
 				data++, tok++;
 
-			program[ip++] = make_instruction(INSTR_PUTCH, data, offset);
+			code[ip++] = make_instruction(INSTR_PUTCH, data, offset);
 
 			while (*tok == '<' || *tok == '>') {
 				if (*tok == '<') offset--;
@@ -415,19 +379,19 @@ void contract()
 	}
 
 	if (offset)
-		program[ip++] = make_instruction(INSTR_ADDPTR, offset, -1);
+		code[ip++] = make_instruction(INSTR_ADDPTR, offset, -1);
 }
 
 int is_clearloop()
 {
 	if (!strncmp(tok, "[-]", 3) || !strncmp(tok, "[+]", 3)) {
-		program[ip++] = make_instruction(INSTR_CLEAR, -1, -1);
+		code[ip++] = make_instruction(INSTR_CLEAR, -1, -1);
 		tok += 3;
 		return 1;
 	}
 
 	int data = 0;
-	char* c = tok + 1;
+	char *c = tok + 1;
 	while (*c != ']') {
 		if (*c != '+' && *c != '-')
 			return 0;
@@ -436,22 +400,24 @@ int is_clearloop()
 		c++;
 	}
 
-	program[ip++] = make_instruction(INSTR_DCLEAR, data, -1);
+	code[ip++] = make_instruction(INSTR_DCLEAR, data, -1);
 	tok = c + 1;
 
 	return 1;
 }
 
-void compile(char* src)
+void compile(char *src)
 {
-	/* in a worst case scenario, none of our optimizations apply,
-	 * and we need an entire Instruction to represent each token */
-	program = bfi_malloc(sizeof(Instruction) * strlen(src));
+	/*
+	 * In a worst case scenario, none of our optimizations apply,
+	 * and we need an entire instruction to represent each token.
+	 */
+	code = malloc(sizeof (*code) * strlen(src));
 
 	tok = src;
 	while (*tok) {
 		int type = command_type(*tok);
-		
+
 		if (*tok == '[') {
 			if (muliplication_loop()) {
 				continue;
@@ -459,35 +425,33 @@ void compile(char* src)
 				continue;
 			} else {
 				stack[sp++] = ip;
-				program[ip++] = make_instruction(type, -1, -1);
+				code[ip++] = make_instruction(type, -1, -1);
 				tok++;
 			}
 		} else if (type >= 6) { /* handles [ and ] (which are not contractable) */
 			if (*tok == ']') {
 				if (sp == 0)
-					fatal_error("Unmatched ].\n");
-				
-				program[ip] = make_instruction(type, stack[--sp], -1);
-				program[stack[sp]].data = ip++;
+					die("Unmatched ].\n");
+
+				code[ip] = make_instruction(type, stack[--sp], -1);
+				code[stack[sp]].data = ip++;
 			} else
-				program[ip++] = make_instruction(type, -1, -1);
-			
+				code[ip++] = make_instruction(type, -1, -1);
+
 			tok++;
 		} else /* token is of type + - , . < or > */
 			contract();
 	}
 
-	program[ip++].type = INSTR_END;
-	program = bfi_realloc(program, sizeof(Instruction) * ip);
+	code[ip++].type = INSTR_END;
+	code = realloc(code, sizeof (*code) * ip);
 
-	if (sp)
-		fatal_error("Unmatched [.\n");
+	if (sp) die("Unmatched [.\n");
 }
 
 /* EXECUTION =============================================================== */
-#define MAX_MEMORY 65536
-char memory[MAX_MEMORY];
-unsigned short ptr; /* free memory wrapping */
+uint8_t memory[1 << 16];
+uint16_t ptr;
 
 /* borrowed from https://github.com/rdebath/Brainfuck/blob/master/bf2any/bf2run.c */
 #if defined(__GNUC__) && ((__GNUC__>4) || (__GNUC__==4 && __GNUC_MINOR__>=4))
@@ -498,92 +462,102 @@ void execute()
 {
 	ip = 0;
 
-	while (1) {
-		switch (program[ip].type) {
-			case INSTR_ADD:
-				memory[(unsigned short)(ptr + program[ip].offset)] += program[ip].data; break;
-			case INSTR_SUB:
-				memory[(unsigned short)(ptr + program[ip].offset)] -= program[ip].data; break;
-			case INSTR_SUBPTR:
-				ptr -= program[ip].data; break;
-			case INSTR_ADDPTR:
-				ptr += program[ip].data; break;
-			case INSTR_PUTCH:
-				for (int i = 0; i < program[ip].data; i++)
-					putchar(memory[(unsigned short)(ptr + program[ip].offset)]); break;
-			case INSTR_GETCH: {
-				char c = getchar();
-				if (c == EOF) {
-					memory[(unsigned short)(ptr + program[ip].offset)] = 0;
-				} else {
-					memory[(unsigned short)(ptr + program[ip].offset)] = c;
-				}
-			} break;
-			case INSTR_CJUMP:
-				if (!memory[ptr])
-					ip = program[ip].data; break;
-			case INSTR_JUMP:
-				ip = program[ip].data - 1; break;
-			case INSTR_CLEAR:
-				memory[ptr] = 0; break;
-			case INSTR_MUL:
-				memory[(unsigned short)(ptr + program[ip].offset)] += memory[ptr] * program[ip].data; break;
-			case INSTR_END:
-				return;
-			case INSTR_DCLEAR:
-				if (program[ip].data % 3 == 0) memory[ptr] = 0;
-				else if ((program[ip].data / 2 % 2 == 0) && (memory[ptr] / 2 % 2 == 0)) memory[ptr] = 0;
-				else { printf("\nbfi: program has entered an infinite loop.\n"); return; }
-				break;
-		}
+loop:
+	switch (code[ip].type) {
+	case INSTR_ADD:
+		memory[(uint16_t)(ptr + code[ip].offset)] += code[ip].data;
+		break;
 
-		ip++;
+	case INSTR_SUB:
+		memory[(uint16_t)(ptr + code[ip].offset)] -= code[ip].data;
+		break;
+
+	case INSTR_SUBPTR:
+		ptr -= code[ip].data; break;
+
+	case INSTR_ADDPTR:
+		ptr += code[ip].data; break;
+
+	case INSTR_PUTCH:
+		for (int i = 0; i < code[ip].data; i++) {
+			putchar(memory[(uint16_t)(ptr + code[ip].offset)]);
+		}
+		break;
+
+	case INSTR_GETCH: {
+		char c = getchar();
+		if (c == EOF) {
+			memory[(uint16_t)(ptr + code[ip].offset)] = 0;
+		} else {
+			memory[(uint16_t)(ptr + code[ip].offset)] = c;
+		}
+	} break;
+
+	case INSTR_CJUMP:
+		if (!memory[ptr]) {
+			ip = code[ip].data;
+		}
+		break;
+
+	case INSTR_JUMP:
+		ip = code[ip].data - 1;
+		break;
+
+	case INSTR_CLEAR:
+		memory[ptr] = 0;
+		break;
+
+	case INSTR_MUL:
+		memory[(uint16_t)(ptr + code[ip].offset)] += memory[ptr] * code[ip].data;
+		break;
+
+	case INSTR_END:
+		return;
+
+	case INSTR_DCLEAR:
+		if (code[ip].data % 3 == 0)
+			memory[ptr] = 0;
+		else if ((code[ip].data / 2 % 2 == 0)
+		         && (memory[ptr] / 2 % 2 == 0))
+			memory[ptr] = 0;
+		else
+			die("\nbfi: program has entered an infinite loop.\n");
+		break;
 	}
+
+	ip++;
+	goto loop;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	char *input_path = NULL;
-	int print_time = 0;
-	
-	for (int i = 1; i < argc; i++) {
-		if (!strcmp(argv[i], "-t"))
-			print_time = 1;
-		else {
-			if (input_path)
-				fatal_error("Usage: bfi INPUT_FILE -oOUTPUT_FILE\n");
 
+	for (int i = 1; i < argc; i++) {
+		if (input_path)
+			die(USAGE);
+		else
 			input_path = argv[i];
-		}
 	}
 
-	if (!input_path) /* we never recieved an input path */
-		fatal_error("Usage: bfi INPUT_FILE -oOUTPUT_FILE\n");
+	if (!input_path) die(USAGE);
 
-	char* src = load_file(input_path);
-
-	if (!src)
-		fatal_error("could not load file.\n");
+	char *src = load_file(input_path);
+	if (!src) die("could not load file.\n");
 
 	sanitize(src);
-
 	compile(src);
 	free(src);
-	
+
 #ifdef DEBUG
-	printf("instruction listing:\n");
+	DBG("instruction listing:\n");
 	for (int i = 0; i < ip; i++)
-		printf("%d: [%s][%5d][%5d]\n", i, instr_strings[program[i].type], program[i].offset, program[i].data);
-	printf("program output:\n");
+		DBG("%d: %s %5d, %5d\n", i, instr_str[code[i].type], code[i].offset, code[i].data);
+	DBG("program output:\n");
 #endif
 
-	clock_t begin = clock();
 	execute();
-	free(program);
-	clock_t end = clock();
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	if (print_time)
-		printf("\nProgram used %f seconds of processor time.\n", time_spent);
+	free(code);
 
 	return EXIT_SUCCESS;
 }
